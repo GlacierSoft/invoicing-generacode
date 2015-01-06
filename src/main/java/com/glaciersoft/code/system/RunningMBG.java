5a55b3afe15609f7280da844381049ad09574e95
dongxichao <406592176@qq.com>
2015-01-06 17:40:33 +0800

    搭建自动创建类工具。

---
+++ src/main/java/com/glaciersoft/code/system/RunningMBG.java
@@ -0,0 +1,51 @@
+package com.glaciersoft.code.system;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+public class RunningMBG {
+    
+    public static String runXMLAUTHORITY = "system/generacode.xml";
+    
+	public static void main(String[] args) {
+		try {
+			List<String> warnings = new ArrayList<String>();
+			boolean overwrite = true;
+			File configFile = new File(ClassLoader.getSystemResource(runXMLAUTHORITY).getFile());
+			ConfigurationParser cp = new ConfigurationParser(warnings);
+			Configuration config = cp.parseConfiguration(configFile);
+			DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+			MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+					callback, warnings);
+			myBatisGenerator.generate(null);
+			for (String warning : warnings) {
+				System.out.println("Warning:" + warning);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
